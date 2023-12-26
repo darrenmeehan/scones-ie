d5c6e9022e9a0d85fee851b00b232ad6d166f377
Darren Meehan <411136+darrenmeehan@users.noreply.github.com>
2023-12-26 06:52:01 +0000

    Listen on all ips

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     let app = Router::new().route("/", get(handler));
 
     // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
