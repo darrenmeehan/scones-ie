0b46504604ac8817dd565cb003366f6c9da77095
Darren Meehan <hi@drn.ie>
2024-06-06 16:34:44 +0100

    Simplify routing

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 use axum::{routing::get, Router};
 use deadpool_diesel::{Manager, Pool};
 use diesel::PgConnection;
-use tower_http::services::ServeFile;
+use tower_http::services::ServeDir;
 
 mod configuration;
 pub mod database;
 mod github;
 mod handlers;
 pub mod models;
-pub mod schema;
 pub mod relme;
+pub mod schema;
 use crate::configuration::get_configuration;
 use crate::database::connect;
 use crate::github::callback_handler;
@@ -29,29 +29,16 @@ pub async fn run() {
 pub fn app(pool: Pool<Manager<PgConnection>>) -> Router {
     Router::new()
         .route("/healthcheck", get(healthcheck_handler))
-        .route_service("/", ServeFile::new("static/index.html"))
-        .route_service(
-            "/admin/config.yml",
-            ServeFile::new("static/admin/config.yml"),
-        )
-        .route_service(
-            "/admin/index.html",
-            ServeFile::new("static/admin/index.html"),
-        )
-        .route_service("/success", ServeFile::new("static/success.html"))
-        .route_service("/logo.jpg", ServeFile::new("static/logo.jpg"))
-        .route_service("/darren.jpg", ServeFile::new("static/darren.jpg"))
-        .route_service("/login", ServeFile::new("static/login.html"))
-        .route_service("/darren", ServeFile::new("static/profile.html"))
+        .nest_service("/", ServeDir::new("static"))
         .route(
             "/.well-known/oauth-authorization-server",
             get(metadata_handler),
         )
-        .route("/metadata", get(metadata_handler))
-        .route("/auth", get(authorization_handler))
-        .route("/callback", get(callback_handler))
-        .route("/token", get(token_handler))
-        .route("/client", get(client_handler))
-        .route("/error", get(error_handler))
+        .route("/api/metadata", get(metadata_handler))
+        .route("/api/auth", get(authorization_handler))
+        .route("/api/callback", get(callback_handler))
+        .route("/api/token", get(token_handler))
+        .route("/api/client", get(client_handler))
+        .route("/api/error", get(error_handler))
         .with_state(pool)
 }
